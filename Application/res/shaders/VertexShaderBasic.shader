dd683df388555f66e99c75f21669bdcbf0ad9107
GutstheHawk <91168846+GutstheHawk@users.noreply.github.com>
2023-03-20 10:27:15 -0500

    Demo build with wireframe cube

--- Application/res/shaders/VertexShaderBasic.shader
+++ Application/res/shaders/VertexShaderBasic.shader
@@ -5,5 +5,6 @@ layout(location = 0) in vec4 position;
 uniform mat4 u_PVM;
 
 void main() {
+
     gl_Position = u_PVM * position;
 }
--- Application/src/Application.cpp
+++ Application/src/Application.cpp
@@ -130,39 +130,62 @@ int main(void)
 
     float positions[] = {
         //front face
-        -0.5f, -0.5f, 0.0f, 1.0f, //0
-         0.5f, -0.5f, 1.0f, 0.0f, //1
-         0.5f,  0.5f, 1.0f, 1.0f, //2
-        -0.5f,  0.5f, 0.0f, 1.0f  //3
+        -0.5f, -0.5f, 0.0f,
+         0.5f, -0.5f, 0.0f,
+         0.5f,  0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f,
+        
+        //back face
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+        -0.5f,  0.5f, -0.5f
 
     };
 
     unsigned int indices[] = {
-        //front face
-        0, 1, 2,
-        2, 3, 0
+	   /* 0, 1, 2,
+		2, 3, 0*/
+		// front
+		0, 1, 2,
+		2, 3, 0,
+		// right
+		1, 5, 6,
+		6, 2, 1,
+		// back
+		7, 6, 5,
+		5, 4, 7,
+		// left
+		4, 0, 3,
+		3, 7, 4,
+		// bottom
+		4, 5, 1,
+		1, 0, 4,
+		// top
+		3, 2, 6,
+		6, 7, 3
 
     };
 
 	glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
-    VertexBuffer vb(positions, 4 * 4 * sizeof(float));
+    VertexBuffer vb(positions, 8 * 3 * sizeof(float));
     
     VertexBufferLayout layout;
-    layout.Push<float>(2, GL_FALSE);
-	layout.Push<float>(2, GL_FALSE);
+    layout.Push<float>(3, GL_FALSE);
+	//layout.Push<float>(2, GL_FALSE);
 
     VertexArray va;
     va.AddBuffer(vb, layout);
     va.Bind();
 
-    IndexBuffer ib(indices, 6);
+    IndexBuffer ib(indices, 36);
     ib.Bind();
 
     //Shader Code
-    std::string vertexShader = ParseShader("res/shaders/vertexShader.shader");
-    std::string fragmentShader = ParseShader("res/shaders/fragmentShader.shader");
+    std::string vertexShader = ParseShader("res/shaders/vertexShaderBasic.shader");
+    std::string fragmentShader = ParseShader("res/shaders/fragmentShaderBasic.shader");
     unsigned int shader = CreateShader(vertexShader, fragmentShader);
     glLinkProgram(shader);
     glUseProgram(shader);
@@ -173,36 +196,39 @@ int main(void)
     glfwSetKeyCallback(window, keyCallback);
     //glBindBuffer(GL_ARRAY_BUFFER, 0);
 
-    Texture texture("res/textures/TheChernoLogo.png");
-    texture.Bind();
+   /*Texture texture("res/textures/TheChernoLogo.png");
+	texture.Bind();
 	int texLocation = glGetUniformLocation(shader, "u_Texture");
-    if (texLocation == -1)
-    {
-        printf("Warning: uniform texLocation doesn't exist!\n");
-    }
-    printf("TexLocation: %d\n", texLocation);
-    glUniform1i(texLocation, 0);
+	if (texLocation == -1)
+	{
+		printf("Warning: uniform texLocation doesn't exist!\n");
+	}
+	printf("TexLocation: %d\n", texLocation);
+	glUniform1i(texLocation, 0);*/
 
-    //glEnable(GL_DEPTH_TEST);
+    glEnable(GL_DEPTH_TEST);
 
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /*Render here */
         //glClearColor(250.0f / 255.0f, 119.0f / 255.0f, 110.0f / 255.0f, 1.0f);
+        double current_time = glfwGetTime();
 
-		/*glm::mat4 pvm = cam->ReturnPVM();
+		glm::mat4 pvm = cam->ReturnPVM();
 
-		int location = glGetUniformLocation(shader, "u_PVM");
-        assert(location == -1);
-		glUniformMatrix4fv(location, 1, GL_FALSE, &pvm[0][0]);*/
+        int location = glGetUniformLocation(shader, "u_PVM");
+		glUniformMatrix4fv(location, 1, GL_FALSE, &pvm[0][0]);
+
+        int timeLoc = glGetUniformLocation(shader, "uTime");
+        //glUniform1f(timeLoc, current_time);
 
         //glClear(GL_COLOR_BUFFER_BIT);
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
